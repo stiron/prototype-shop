0d0cfb16619d4631bf8059fe4766c108f76faa30
Seong Chol Lee <58536613+stiron@users.noreply.github.com>
2023-06-04 19:00:00 +0900

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        &copy; <span class="this-year"></span>2023 Stiron Lee. All rights
+        &copy; 2023 Stiron Lee. All rights
         reserved.
       </p>
     </footer>
